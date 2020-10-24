20b4fdb2f60b976cd6bf2f079c724a0c416a253a
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-10-24 12:03:08 -0300

    arbol y desafio listo

--- patrones.rb
+++ patrones.rb
@@ -99,19 +99,36 @@ def numero_0(x)
 end
 
 #método para árbol de navidad
-def navidad(x)
+=begin def navidad(x)
   (x + 2).times do |i|
     i += 1
-    #parte de arriba
     (x + 4).times do |j|
       j += 1
-      if i + j == x || (j == (x - 1) && (i + j) < (x + 4)) || ((j - i) < x && (i + j) == (x + 2) && i < x) || ((j - i) < x && (i + j) == (x + 4) && i < x) || ((j - i) < x && (i + j) == (x + 6) && i < x) || j == (x - 1) || (i == (x + 2) && (i + j).odd? && (i - j) > 0) 
+      if i + j == x || (j == (x - 1) && (i + j) < (x + 4)) || ((j - i) < x && (i + j) == (x + 2) && i < x) || ((j - i) < x && (i + j) == (x + 4) && i < x) || ((j - i) < x && (i + j) == (x + 6) && i < x) || j == (x - 1) || (i == (x + 2) && (i + j).odd? && (i - j) > 0) || (i == 5 && j == 9)
         print '*'
       else
         print ' '
       end
     end
-    #parte de abajo
+    print "\n"
+  end
+  print "\n"
+end
+=end
+
+def navidad(x)
+  size_x = (x - 1) * 2
+  size_y = (x - 1) * 2 - 1
+  (1..size_y).each do |i|
+    (1..size_x).each do |j|
+      if (j == (size_x / 2) + 1 && i >= x) || # bottom vertical line
+         (i == size_y && j > 1 && j < size_x && j.odd?) || # bottom horizontal line
+         (i < x && j > (size_x / 2) + 1 - i && j < (size_x / 2) + 1 + i) && (i.odd? && j.odd? || i.even? && j.even?) # tree
+        print '*'
+      else
+        print ' '
+      end
+    end
     print "\n"
   end
   print "\n"
